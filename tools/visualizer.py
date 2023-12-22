57df30a0d858e4fc77e6b3feb3b210a58e297e06
Krystian <krystiano1602@gmail.com>
2023-12-22 10:06:32 +0100

    write_edges_connecting_researchers_to_csv method implemented

--- tools/visualizer.py
+++ tools/visualizer.py
@@ -1,3 +1,5 @@
+import psycopg2
+import csv
 class Visualizer:
     def __init__(self):
         pass
@@ -5,8 +7,28 @@ class Visualizer:
     def write_researchers_nodes_to_csv(self):
         pass
 
-    #TODO
+
     def write_edges_connecting_researchers_to_csv(self):
-        pass
+        conn = psycopg2.connect("host = localhost user=postgres password=1234 port=5432")
+        cur = conn.cursor()
+        cur.execute("Select DISTINCT researchers.id, articles.id from "
+                    "researchers inner join links "
+                    "on researchers.id = links.researcher_id inner join articles on "
+                    "links.article_id = articles.id WHERE researchers.id < articles.id;")
+
+        row = cur.fetchone()
+
+        with open("edges.csv", 'w', newline='') as file:
+            csvWriter = csv.writer(file)
+
+            while row is not None:
+                csvWriter.writerow(row)
+                row = cur.fetchone()
 
 
+        cur.close()
+        conn.close()
+
+if __name__ == "__main__":
+    visualiser = Visualizer()
+    visualiser.write_edges_connecting_researchers_to_csv()
